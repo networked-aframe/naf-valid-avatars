d946add7b28bd7e65342fd1cf0a12bdb2f04b57c
Vincent Fretin <vincent.fretin@gmail.com>
2024-02-17 15:06:07 +0100

    add isConnected method to NAF type

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,6 +7,7 @@ declare var NAF: {
       enableMicrophone: (enabled: boolean) => void;
     };
     broadcastDataGuaranteed: (dataType: string, data: object) => void;
+    isConnected: () => boolean;
     subscribeToDataChannel: (
       dataType: string,
       callback: (senderId: string, dataType: string, data: object, targetId: string | undefined) => void,
